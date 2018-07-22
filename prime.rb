5c674a3074cbe52bef710a61a45eacade7c24a8a
Richard Chen <richard.c.chen@gmail.com>
2018-07-22 23:16:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,22 @@
 # Add  code here!
 def prime(int)
+
   if int <= 0 
     return false
+  
   else
-  checker = []
-  adder = 0
-  while adder < int
-    checker[adder] = adder
-    adder += 1
-  end
-  checker.each do |i|
-    if int%i = 0
-      return false
+    checker = []
+    adder = 0
+    while adder < int
+      checker[adder] = adder
+      adder += 1
+    end
+    checker.each do |i|
+      if int%i = 0
+        return false
+      end
     end
   return true
+  
   end
 end
