16bce9116a122cbdceba978deb0b74b0d73c1eab
Richard Chen <richard.c.chen@gmail.com>
2018-07-22 23:32:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(int)
+
+  if int <= 1
+    return false
+  elsif int == 2
+    return true
+  else
+    checker = [2]
+    adder = 3
+    while adder < int
+      checker.push(adder)
+      adder += 1
+    end
+    checker.each do |i|
+      if int%i == 0
+        return false
+      end
+    end
+    
+  return true
+  
+  end
+end
