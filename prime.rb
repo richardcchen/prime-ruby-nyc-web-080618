ea673bd99e01036365830b492693088366657900
Richard Chen <richard.c.chen@gmail.com>
2018-07-22 23:09:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,13 @@
 # Add  code here!
 def prime(int)
+  if int <= 0 
+    return false
+  else
   checker = []
+  intcount = 1..int
+  for counter in intcount
+    checker[intcount-1] = 
+
   
+  end
 end
